0fe0fac6ef02f1b45e7cae6454702ee90d2c5a38
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-05-27 19:44:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+          <Route index element={< Layout />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
